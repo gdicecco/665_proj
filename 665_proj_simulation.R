6f0c4de4017b47f51c5f979442ecc8ae0f491c11
Di Cecco <gdicecco@ad.unc.edu>
2018-03-30 15:37:46 -0400

    add code for JAGS models (fixed and hierarchical) for simulated data

--- 665_proj_simulation.R
+++ 665_proj_simulation.R
@@ -1,11 +1,12 @@
 ## 665 project simulation
 library(dplyr)
+source("clarkFunctions2018.r")
 
 #simulate dataset for one species
-data <- data.frame("year" = sample(seq(1990, 2000, by = 1), 100, replace = T),
-                   "route" = sample(seq(1,10, by = 1), 100, replace = T),
+data <- data.frame("year" = sample(seq(1990, 2000, by = 1), 1000, replace = T),
+                   "route" = sample(seq(1,10, by = 1), 1000, replace = T),
                    "count" = rpois(100, 2), 
-                   "observer" = sample(seq(1,20, by = 1), 100, replace = T))
+                   "observer" = sample(seq(1,20, by = 1), 1000, replace = T))
   
 #observer-route ID
 data$obsroute <- paste0(data$observer, data$route, sep = "")
@@ -34,8 +35,9 @@ data <- data %>%
 
 #JAGS model of counts with random effects for year and obsroute
 i <- data$strata
-t <- data$year
 j <- data$obsroute
+t <- data$year - 1989
+data$t <- t
 
 #Model format
 #log(counts) = Si + Bt(t - t*) + wj + nI(j,t)
@@ -52,9 +54,75 @@ j <- data$obsroute
 #B ~ N(0, 1000) - strata specific slope
 #n ~ N(0, 1000) - first year observer
 
-counts <- model.frame(count ~ strata + year + obsroute + firstyr, data)
-X <- model.matrix(count ~ strata + year + obsroute + firstyr, counts)
+#ID observers
+z <- as.character(data$obsroute)
+zall <- sort(unique(z))
+ix <- match(z, zall)
+data$ix <- ix
+
+counts <- model.frame(count ~ strata + t + ix + firstyr, data)
+X <- model.matrix(count ~ strata + t + ix + firstyr, counts)
 y <- counts$count
 n <- length(y)
 
+#JAGS fixed effects
+cat("model{
+    for(i in 1:n){
+      y[i] ~ dpois(lambda[i])
+      lambda[i] <- exp(b1*X[i, 2] + b2*X[i, 3] + b3*X[i, 5])
+    }
+
+    b1 ~ dnorm(0.0, 1000)
+    b2 ~ dnorm(0.0, 1000)
+    b3 ~ dnorm(0.0, 1000)
+    }
+    ", fill = T, file = "countsFixed.txt"
+    )
+
+library(R2jags)
+countData <- list(y = y, X = X, n = n)
+parNames <- c("b1", "b2", "b3")
+countFit <- jags(data = countData, param = parNames,
+                 n.iter = 5000, n.burnin = 1000, model.file = "countsFixed.txt")
+print(countFit)
+
+par(mar=c(3,3,1,1))
+plot( as.mcmc(countFit) )
+
+#JAGS hierarchical
+nobs <- max(ix)            # no. observers
+
+cat("model{
+    for(i in 1:n){
+    y[i] ~ dpois(lambda[i])
+    lambda[i] <- exp(b1*X[i, 2] + b2*X[i, 3] + b3*X[i, 5] + a1[ix[i]]*X[i, 4])
+    }
+    
+    for(j in 1:nobs){
+      a1[j] ~ dnorm(0.0, sigma)
+    }
+    
+    sigma ~ dgamma(0.001, 1000)
+    b1 ~ dnorm(0.0, 1000)
+    b2 ~ dnorm(0.0, 1000)
+    b3 ~ dnorm(0.0, 1000)
+    }
+    ", fill = T, file = "countsRE.txt"
+)
+
+countData   <- list(y = y, X = X, ix = ix, nobs = nobs, n = n)
+
+parNamesRE <- c('b1','b2','b3','a1','sigma')
+
+parInit <- function(){ 
+  list(b1 = rnorm(1,0,1), b2 = rnorm(1,0,1), b3 = rnorm(1,0,1), sigma = runif(1,0,1),
+       a1 = rnorm(nobs,0,1)) 
+}
+
+countRE <- jags(data=countData, inits=parInit, param=parNamesRE,
+                 n.iter=2000, n.burnin=100, model.file="countsRE.txt")
+rePars <- getJagsPars(countRE)
+fixed  <- rePars$fixed
+beta   <- fixed[1:4,1] #get very very small with random effect
+alpha  <- rePars$mean
 
