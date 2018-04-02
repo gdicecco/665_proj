f625c7603ef1d16b1a1121c6fa0013b7909b6430
Di Cecco <gdicecco@ad.unc.edu>
2018-04-02 16:28:58 -0400

    add fixed and re models in jags for data, test on northern bobwhite

--- 665_proj_model.R
+++ 665_proj_model.R
@@ -8,6 +8,8 @@ library(maps)
 library(rgdal)
 library(rgeos)
 
+source("clarkFunctions2018.r")
+
 #Read in BBS data
 ##Note: on mac once connected to Bioark server, path is "/Volumes/hurlbertlab/Databases/BBS/2017/filename.csv"
 routes <- read.csv("\\\\Bioark.bio.unc.edu\\hurlbertlab\\Databases\\BBS\\2017\\bbs_routes_20170712.csv")
@@ -31,7 +33,8 @@ counts$stateroute <- counts$statenum*1000 + counts$route
 #Subset counts by species
 counts.short <- counts %>%
   filter(year >= 1969) %>%
-  filter(aou %in% species$aou)
+  #filter(aou %in% species$aou)
+  filter(aou == 2890)
 counts.merge <- merge(routes.short, counts.short, by = c("stateroute", "year"))
 
 #Add obs-route ID and dummy variable for first year observers
@@ -39,7 +42,7 @@ counts.merge$obsroute <- paste0(counts.merge$observer, counts.merge$route, sep =
 
 counts.merge <- arrange(counts.merge, year)
 
-#Loop takes 1 hour
+#Loop takes some time
 init.time = Sys.time()
 firstyr <- c(1)
 for(i in 2:nrow(counts.merge)) {
@@ -57,5 +60,103 @@ for(i in 2:nrow(counts.merge)) {
   print(paste(i, "out of", nrow(counts.merge), "; current time:", curr.time,
               "; estimated end time:", estimated.end))
 }
-
+#1 spp - about 10 minutes
 counts.merge$firstyr <- firstyr
+
+Sys.time()
+
+#JAGS model of counts with random effects for year and obsroute
+s <- as.character(paste0(counts.merge$statenum.x, counts.merge$bcr, sep = ""))
+sall <- sort(unique(s))
+i <- match(s, sall)
+counts.merge$strata <- i
+
+j <- counts.merge$obsroute
+t <- counts.merge$year - 1968
+counts.merge$t <- t
+
+#ID observers
+z <- as.character(counts.merge$obsroute)
+zall <- sort(unique(z))
+ix <- match(z, zall)
+counts.merge$ix <- ix
+
+counts <- model.frame(stoptotal ~ strata + t + ix + firstyr, counts.merge)
+X <- model.matrix(stoptotal ~ strata + t + ix + firstyr, counts)
+y <- counts$stoptotal
+n <- length(y)
+
+#JAGS fixed effects
+cat("model{
+    for(i in 1:n){
+    y[i] ~ dpois(lambda[i])
+    lambda[i] <- exp(b1*X[i, 2] + b2*X[i, 3] + b3*X[i, 5])
+    }
+    
+    b1 ~ dnorm(0.0, 1000)
+    b2 ~ dnorm(0.0, 1000)
+    b3 ~ dnorm(0.0, 1000)
+    }
+    ", fill = T, file = "countsFixed.txt"
+)
+
+library(R2jags)
+countData <- list(y = y, X = X, n = n)
+parNames <- c("b1", "b2", "b3")
+countFit <- jags(data = countData, param = parNames,
+                 n.iter = 10000, n.burnin = 2500, model.file = "countsFixed.txt")
+# 1 spp - < 2 hours
+print(countFit)
+countFit.out <- countFit$BUGSoutput$summary
+write.csv(countFit.out, "bobwhite_fixed_jagssummary.csv")
+
+Sys.time()
+
+par(mar=c(3,3,1,1))
+plot( as.mcmc(countFit) )
+
+#JAGS random effects
+
+#JAGS hierarchical
+nobs <- max(ix)            # no. observers
+
+cat("model{
+    for(i in 1:n){
+    y[i] ~ dpois(lambda[i])
+    lambda[i] <- exp(b1*X[i, 2] + b2*X[i, 3] + b3*X[i, 5] + a1[ix[i]]*X[i, 4])
+    }
+    
+    for(j in 1:nobs){
+    a1[j] ~ dnorm(0.0, sigma)
+    }
+    
+    sigma ~ dgamma(0.001, 1000)
+
+    b1 ~ dnorm(0.0, 1000)
+    b2 ~ dnorm(0.0, 1000)
+    b3 ~ dnorm(0.0, 1000)
+    }
+    ", fill = T, file = "countsRE.txt"
+)
+
+countData   <- list(y = y, X = X, ix = ix, nobs = nobs, n = n)
+
+parNamesRE <- c('b1','b2','b3','a1','sigma')
+
+parInit <- function() {
+  list(b1 = rnorm(1,0,1), b2 = rnorm(1,0,1), b3 = rnorm(1,0,1)) 
+}
+
+Sys.time()
+countRE <- jags(data=countData, inits=parInit, param=parNamesRE,
+                n.iter=20000, n.burnin=5000, model.file="countsRE.txt")
+#3.5 hours for bobwhite
+Sys.time()
+rePars <- getJagsPars(countRE)
+fixed  <- rePars$fixed
+beta   <- fixed[1:4,1] 
+alpha  <- rePars$mean
+write.csv(fixed, "bobwhite_jagsRE_fixed.csv", row.names = T)
+write.csv(alpha, "bobwhite_jagsRE_re.csv")
+
+counts.merge$abundind <- exp(beta[1]*counts.merge$strata + beta[2]*counts.merge$t)
