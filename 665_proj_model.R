98144d57c2f0b8a6b7194960bf710692574eaf81
Di Cecco <gdicecco@ad.unc.edu>
2018-04-09 17:02:36 -0400

    updated results, made figures for presentation

--- 665_proj_model.R
+++ 665_proj_model.R
@@ -48,7 +48,7 @@ colnames(dics) <- c("aou", "jags", "dic")
 
 spp15 <- species[1:15, ]
 
-for(i in 7:15) {
+for(i in 1:10) {
   AOU <- spp15$aou[i]
 #Subset counts by species
 counts.short <- bbscounts %>%
@@ -57,7 +57,7 @@ counts.short <- bbscounts %>%
 counts.merge <- merge(routes.short, counts.short, by = c("stateroute", "year"))
 
 #Add obs-route ID and dummy variable for first year observers
-counts.merge$obsroute <- paste0(counts.merge$observer, counts.merge$route, sep = "")
+counts.merge$obsroute <- paste0(counts.merge$obsn, counts.merge$stateroute, sep = "")
 
 counts.merge <- arrange(counts.merge, year)
 
@@ -199,9 +199,116 @@ fixedb2 <- countFit$BUGSoutput$mean$b2
 counts.merge$fixedabundind <- exp(fixedb1[counts.merge$strata] + fixedb2[counts.merge$strata]*counts.merge$t)
 
 results <- rbind(results, counts.merge)
+send_message(text_msg)
 }
 
 write.csv(dics, "DIC_all_spp_models.csv", row.names = F)
 write.csv(results, "counts_w_modeloutput.csv", row.names = F)
-send_message(text_msg)
+
+
+#### Figures for presentation #####
+# delta DIC
+
+delta <- as.data.frame(dics[-1, ]) %>%
+  group_by(aou) %>% 
+  summarize(delta = diff(as.numeric(as.character(dic))))
+
+library(ggplot2)
+theme_set(theme_bw())
+
+ggplot(delta, aes(x = aou, y = log10(abs(delta)))) + geom_bar(stat = "identity", fill = "grey") + 
+  xlab("AOU") +  ylab("log10(Fixed effects DIC - Random effects DIC)")
+
+# distribution of coefficients
+## AOU == 3850
+
+# strata specific intercept
+# 34 strata
+betas <- as.data.frame(exp(beta), row.names = F)
+
+library(cowplot)
+
+# strata specific fixed effects (intercept, slope)
+b1 <- ggplot(betas, aes(x = b1)) + geom_histogram(binwidth = 0.0001, col = "white") +
+  ylab("Number of strata") + xlab("Intercept")
+b2 <- ggplot(betas, aes(x = b2)) + geom_histogram(binwidth = 0.005, col = "white") +
+  ylab("Number of strata") + xlab("Slope")
+plot_grid(b1, b2, nrow = 2)
+
+# compare these from RE model to FE model w/ cred int
+fixed.df <- data.frame(param = row.names(countFit.out), countFit.out, row.names = NULL)
+params <- strsplit(as.character(fixed.df$param), "\\[|\\]")
+params.df <- data.frame(matrix(unlist(params[1:68]), nrow = 68, byrow = T))
+fixedbetas <- data.frame(param = params.df$X1, strata = params.df$X2, mean = exp(fixed.df$mean[1:68]), 
+                         cilow = exp(fixed.df$X2.5.[1:68]), cihi = exp(fixed.df$X97.5.[1:68]))
+
+paramnames <- c("b1" = "Intercept", "b2" = "Slope")
+fixplot <- ggplot(fixedbetas, aes(x = as.numeric(strata), y = mean)) + geom_point() + ylim(0.9, 1.3) + 
+  geom_errorbar(aes(ymin = cilow, ymax = cihi)) + ylab("Effect size") + xlab(" ") + 
+  facet_grid(~param, labeller = as_labeller(paramnames))
+
+random.df <- data.frame(param = row.names(countRE$BUGSoutput$summary), countRE$BUGSoutput$summary, row.names = NULL)
+paramre <- strsplit(as.character(random.df$param), "\\[|\\]")
+paramre.df <- data.frame(matrix(unlist(paramre[1:1288]), nrow = 1288, byrow = T))
+paramre.betas <- filter(paramre.df, X1 == "b1" | X1 == "b2")
+re.betas12 <- cbind(random.df[1:1288, ], paramre.df)
+randombetas <- filter(re.betas12, X1 == "b1" | X1 == "b2")
+
+replot <- ggplot(randombetas, aes(x = as.numeric(as.character(X2)), y = exp(mean))) + geom_point() + ylim(0.9, 1.3) +
+  geom_errorbar(aes(ymin = exp(X2.5.), ymax = exp(X97.5.))) + ylab("Effect size") + xlab("Strata") + 
+  facet_grid(~X1, labeller = as_labeller(paramnames))
+
+plot_grid(fixplot, replot, nrow = 2, labels = c("(a)", "(b)"))
+
+# random effects: observer
+# 1220 obsroute, 747 observers
+alphas <- exp(alpha)
+alph.df <- data.frame(alpha = alphas)
+
+#histogram of random effects
+a1 <- ggplot(alph.df, aes(x = alpha)) + geom_histogram(binwidth = 0.05, col = "white") + ylab("Number of observers") + 
+  xlab("Random effect")
+a1
+# 502 routes, 3790 observations (route x year)
+
+#point plot of random effects
+alphapoints <- ggplot(random.df[1:1220, ], aes(x = param, y = exp(mean))) + geom_point() + 
+  geom_errorbar(aes(ymin = exp(X2.5.), ymax = exp(X97.5.))) + xlab("Observer x Route ID") + ylab("Effect size") + 
+  theme(axis.text.x = element_blank(), axis.ticks.x = element_blank())
+alphapoints
+
+#years of observations per observer
+obsyrs <- counts.merge %>% group_by(obsroute) %>% distinct(year) %>% summarize(nyears = n())
+
+ggplot(obsyrs, aes(x = nyears)) + geom_histogram(binwidth = 1, col = "white") +
+  xlab("Number of years") + ylab("Frequency")
+
+# First year observer 
+# for which species is there a nonzero overlapping CI 
+setwd("C:/Users/gdicecco/Desktop/git/665_proj/model_output/")
+files <- list.files()
+
+library(stringr)
+sums <- files[str_detect(files, "fixed_")]
+
+firsts <- data.frame(X = NA, mean = NA, X2.5. = NA, X97.5. = NA, aou = NA, model = NA)
+for(i in 1:length(sums)){
+  df <- read.csv(sums[i])
+  char <- str_split(sums[i], "_")
+  subs <- df[df$X == "b3", c("X", "mean", "X2.5.", "X97.5.")]
+  subs$aou <- char[[1]][1]
+  subs$model <- char[[1]][3]
+  firsts <- rbind(firsts, subs)
+}
+
+firsts <- na.omit(firsts)
+firsts$model <- factor(firsts$model, levels = c("jagssummary.csv", "jagsRE.csv"))
+
+modnames <- c("jagssummary.csv" = "Fixed effects", "jagsRE.csv" = "Hierarchical")
+ggplot(firsts, aes(x = aou, y = exp(mean))) + geom_point() + ylab("First year observer effect size") + xlab("AOU") + 
+  geom_errorbar(aes(ymin = exp(X2.5.), ymax = exp(X97.5.))) +
+  facet_grid(~model, labeller = as_labeller(modnames)) + theme_bw()
+
+#### Calculate weighted abund indices and plot centroid movement ####
+
 
